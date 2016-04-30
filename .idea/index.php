652d74511247cb8fe6afb95a21e05e6deaa49a30
Izael Oliveira <izaelos22@gmail.com>
2016-04-30 00:27:24 -0300

    Lista de alunos e nota.

---
+++ .idea/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: izael
+ * Date: 29/04/16
+ * Time: 03:44
+ */
--- conexao.php
+++ conexao.php
@@ -7,8 +7,28 @@ $conexao = new \PDO(
     "phpcompdo",
     "1234"
     );
+	$query = "Select * from alunos order by nome asc";
+
+	$stmt = $conexao->query($query);
+	$resultado = $stmt->fetchAll(PDO::FETCH_CLASS);
+	echo "<table border='1'><caption>TURMA codeEducation</caption>
+	<tr>
+		<td></td>
+		<td>Aluno</td>
+		<td>Nota</td>
+	</tr>";
+	$i=1;
+	foreach($resultado as $aluno){
+
+		echo "<tr><td>".$i++."</td><td>".$aluno->nome."</td><td>".$aluno->nota."</td></tr>";
+}
 }
 catch(\PDOException $e){
     echo "Não foi possivel conectar ao banco de dados. <br>Código Erro:"
     .$e->getCode();
 }
+
+
+
+//select * from alunos order by nota desc limit 3;
+//update alunos set nota=11 where id=11;
