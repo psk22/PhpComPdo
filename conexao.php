016b4b63a83d1a71a9a5142cc40aa72a525eb33c
Izael Oliveira <izaelos22@gmail.com>
2016-05-08 22:12:43 -0300

    Charset=utf-8

--- conexao.php
+++ conexao.php
@@ -1,34 +1,19 @@
 <?php
-
-try {
-$conexao = new \PDO(
-    "mysql:host=localhost;
-    dbname=phpcompdo",
-    "phpcompdo",
-    "1234"
-    );
-	$query = "Select * from alunos order by nome asc";
-
-	$stmt = $conexao->query($query);
-	$resultado = $stmt->fetchAll(PDO::FETCH_CLASS);
-	echo "<table border='1'><caption>TURMA codeEducation</caption>
-	<tr>
-		<td></td>
-		<td>Aluno</td>
-		<td>Nota</td>
-	</tr>";
-	$i=1;
-	foreach($resultado as $aluno){
-
-		echo "<tr><td>".$i++."</td><td>".$aluno->nome."</td><td>".$aluno->nota."</td></tr>";
+	header('Content-Type: text/html; charset=utf-8');
+	function conectar(){
+	try {
+		$conexao = new \PDO(
+			"mysql:host=localhost;dbname=phpcompdo","phpcompdo","1234"
+			);
+		$conexao -> setAttribute(PDO::ATTR_ERRMODE, ERRMODE_EXCEPTION);
+		}
+		catch(\PDOException $e){
+			echo "Não foi possivel conectar ao banco de dados. <br>Código Erro:"
+			.$e->getCode();
+		}
+		header('Content-Type: text/html; charset=utf-8');
+		return $conexao;
+		
 }
-}
-catch(\PDOException $e){
-    echo "Não foi possivel conectar ao banco de dados. <br>Código Erro:"
-    .$e->getCode();
-}
-
-
-
 //select * from alunos order by nota desc limit 3;
 //update alunos set nota=11 where id=11;
