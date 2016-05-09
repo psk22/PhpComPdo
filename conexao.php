9e0c789948cf39a9c967385632800efc9f4f66b7
Izael Oliveira <izaelos22@gmail.com>
2016-05-08 22:51:51 -0300

    seleção por nota

--- conexao.php
+++ conexao.php
@@ -11,9 +11,7 @@
 			echo "Não foi possivel conectar ao banco de dados. <br>Código Erro:"
 			.$e->getCode();
 		}
-		header('Content-Type: text/html; charset=utf-8');
+
 		return $conexao;
 		
 }
-//select * from alunos order by nota desc limit 3;
-//update alunos set nota=11 where id=11;
--- selecao.php
+++ selecao.php
@@ -1,12 +1,21 @@
 <?php
 require_once "conexao.php";
+heade('Content-Type: text/html; charset=utf-8');
+$pdo=conectar();
 
-$pdo=connectar();
+$buscaNota=$pdo->prepare("select * from alunos order by nota desc limit 3");
+$buscaNota->execute();
 
 $busca=$pdo->prepare("SELECT * FROM alunos");
 $busca->execute();
 
+echo "<hr><h12>Lista de Alunos</h2><br>";
 $listar=$busca->fetchAll(PDO::FETCH_CLASS);
 foreach ($listar as $i){
-    echo $i->id," - ".$i->nome." - ".$i->nota;
+    echo $i->id," - ".$i->nome." - ".$i->nota."<br>";
+}
+echo "<hr><h12>Maiores Notas</h2><br>";
+$listaNota=$buscaNota->fetchAll(PDO::FETCH_CLASS);
+foreach ($listaNota as $i2){
+    echo $i2->id," - ".$i2->nome." - ".$i2->nota."<br>";
 }
